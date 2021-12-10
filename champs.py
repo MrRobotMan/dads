f4fbc61584f8396c8983fd97a673c358535a5cd1
David Weiss <dweiss2@gmail.com>
2021-12-09 20:33:50 -0600

    Updated champs for patch 2.6

---
+++ champs.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+from collections import OrderedDict
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--new", type=bool, default=False, help="Add a new champ")
+    args = parser.parse_args()
+    if args.new:
+        champ = input("Enter the champ's name: ")
+        positions = input("Enter the champ's positions (b, m, d, s, j): ")
+        roles = {"b": "Baron", "m": "Mid", "d": "Dragon", "s": "Support", "j": "Jungle"}
+        # builds = input("Enter the champ's builds: ")
+        new_champ = {champ: {"positions": [roles[i] for i in positions], "builds": []}}
+    with open("champs.json", "w") as fs:
+        unordered_champs: dict[str, dict[str, list[str]]] = json.load(fs)
+        if args.new:
+            unordered_champs.update(new_champ)
+        champs = OrderedDict(sorted(unordered_champs.items()))
+        json.dump(champs, fs, indent=2)
+
+
+if __name__ == "__main__":
+    main()
