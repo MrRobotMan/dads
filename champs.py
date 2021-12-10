fec3dcffb6083752325b3b61020ece10a5fb129b
David Weiss <dweiss2@gmail.com>
2021-12-09 20:40:07 -0600

    Champs can now be added and sorted with champs.py

--- champs.py
+++ champs.py
@@ -5,7 +5,9 @@ from collections import OrderedDict
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--new", type=bool, default=False, help="Add a new champ")
+    parser.add_argument(
+        "-n", "--new", action="store_true", default=False, help="Add a new champ"
+    )
     args = parser.parse_args()
     if args.new:
         champ = input("Enter the champ's name: ")
@@ -13,12 +15,13 @@ def main() -> None:
         roles = {"b": "Baron", "m": "Mid", "d": "Dragon", "s": "Support", "j": "Jungle"}
         # builds = input("Enter the champ's builds: ")
         new_champ = {champ: {"positions": [roles[i] for i in positions], "builds": []}}
-    with open("champs.json", "w") as fs:
+    with open("champs.json", "r") as fs:
         unordered_champs: dict[str, dict[str, list[str]]] = json.load(fs)
         if args.new:
             unordered_champs.update(new_champ)
         champs = OrderedDict(sorted(unordered_champs.items()))
-        json.dump(champs, fs, indent=2)
+    with open("champs.json", "w") as fp:
+        json.dump(champs, fp, indent=2)
 
 
 if __name__ == "__main__":
