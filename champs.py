27b8f982ec4e4579e20523bba738556c5b2a0c53
David Weiss <dweiss2@gmail.com>
2022-08-23 09:55:32 -0500

    Fixed adding a champ script

--- champs.py
+++ champs.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 from collections import OrderedDict
 from pathlib import Path
@@ -7,24 +6,18 @@ CHAMPS = Path("champs.json")
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-n", "--new", action="store_true", default=False, help="Add a new champ"
-    )
-    args = parser.parse_args()
-    if args.new:
-        champ = input("Enter the champ's name: ")
+    with CHAMPS.open() as fs:
+        unordered_champs: dict[str, list[str]] = json.load(fs)
+    while champ := input("Enter the champ's name (enter blank to exit): "):
         positions = input("Enter the champ's positions (b, m, d, s, j): ")
         roles = {"b": "Baron", "m": "Mid", "d": "Dragon", "s": "Support", "j": "Jungle"}
         # builds = input("Enter the champ's builds: ")
         # new_champ = {champ: {"positions": [roles[i] for i in positions], "builds": []}}
-        new_champ = {champ: [roles[i] for i in positions]}
-        with CHAMPS.open() as fs:
-            unordered_champs: dict[str, list[str]] = json.load(fs)
-            unordered_champs.update(new_champ)
-            champs = OrderedDict(sorted(unordered_champs.items()))
-        with CHAMPS.open("w") as fp:
-            json.dump(champs, fp, indent=2)
+        new_champ = {champ: [roles[i] for i in sorted(positions)]}
+        unordered_champs.update(new_champ)
+    champs = OrderedDict(sorted(unordered_champs.items()))
+    with CHAMPS.open("w") as fp:
+        json.dump(champs, fp, indent=2)
 
 
 if __name__ == "__main__":
