e6ab99e2d1a45d644e0502d2e5ddf33b69ee0198
David Weiss <dweiss2@gmail.com>
2022-08-23 22:02:03 -0500

    Finished testing. Bot works

--- dadbot.py
+++ dadbot.py
@@ -6,7 +6,7 @@ import logging
 import random
 from collections import defaultdict
 from pathlib import Path
-from typing import DefaultDict
+from typing import DefaultDict, Optional
 
 import discord
 import discord.ext.commands as commands
@@ -78,7 +78,7 @@ def make_chaos(champs: list[str]) -> list[str]:
     return [f"{builds[i]} {positions[i]} {champ}" for i, champ in enumerate(team)]
 
 
-def get_user_timeout_data(user: discord.Member) -> tuple[int, int]:
+def get_user_timeout_data(user: discord.Member) -> tuple[int, int, str]:
     """Checks if the indicated user is in timeout
 
     Args:
@@ -88,27 +88,29 @@ def get_user_timeout_data(user: discord.Member) -> tuple[int, int]:
         tuple[int, int]: Number of times in timeout and duration (seconds)
     """
     with TIMEOUTS.open() as fp:
-        data: dict[str, tuple[int, int]] = json.load(fp)
-    return data.get(user.name, (0, 0))
+        data: dict[str, tuple[int, int, str]] = json.load(fp)
+    return data.get(user.name, (0, 0, user.name))
 
 
-def get_timeout_leaderboard() -> tuple[str, str]:
+def get_timeout_leaderboard() -> Optional[tuple[str, str]]:
     """Return the most timed out people.
 
     Returns:
         tuple[list[tuple[int, str]]]: users and number of timeouts / total time.
     """
     with TIMEOUTS.open() as fs:
-        data: dict[discord.Member, tuple[int, int]] = json.load(fs)
-    reversed_data = {v: k for k, v in data.items()}
+        data: dict[str, tuple[int, int, str]] = json.load(fs)
+    if not data:
+        return None
+    reversed_data = {(v[0], v[1]): v[2] for v in data.values()}
     timed_out_qty = sorted(list(reversed_data.items()), key=lambda x: x[0][0])[:3]
     timed_out_time = sorted(list(reversed_data.items()), key=lambda x: x[0][1])[:3]
-    longest_name = max(len(user.name) for user in data.keys())
+    longest_name = max(len(user[2]) for user in data.values())
     most_timed_out = "\n".join(
-        f"{user[1].mention:{longest_name}} | {user[0][0]}" for user in timed_out_qty
+        f"{user[1]:{longest_name}} | {user[0][0]}" for user in timed_out_qty
     )
     longest_timed_out = "\n".join(
-        f"{user[1].mention:{longest_name}} | {user[0][1]}" for user in timed_out_time
+        f"{user[1]:{longest_name}} | {user[0][1]}" for user in timed_out_time
     )
     return (most_timed_out, longest_timed_out)
 
@@ -123,7 +125,11 @@ def main() -> None:
 
     champs, champ_positions = get_champs()
 
-    intents = discord.Intents(messages=True, members=True, message_content=True)
+    intents = discord.Intents(
+        messages=True,
+        members=True,
+        message_content=True,
+    )
     bot = commands.Bot(command_prefix="!", intents=intents)
 
     @bot.command(name="team", help="Responds with a random team")
@@ -172,27 +178,31 @@ def main() -> None:
             # Show a user or multiple users
             response: list[str] = []
             for user in args:
-                timeouts, total_time = get_user_timeout_data(user)
+                timeouts, total_time, _ = get_user_timeout_data(user)
                 hours, minutes = divmod(total_time, 3600)
-                minutes, seconds = divmod(minutes, 36)
+                minutes, seconds = divmod(minutes, 60)
                 response.append(
                     f"{user.mention} has been in timeout {timeouts} times for {hours}:{minutes}:{seconds}."
                 )
         else:
             # Show the leaderboard
             leaderboard = get_timeout_leaderboard()
-            padding = max(
-                len(user) for user in itertools.chain(leaderboard[0], leaderboard[1])
-            )
-            response = [
-                "Most timed out:",
-                "-" * padding,
-                leaderboard[0],
-                "-" * padding,
-                "Longest timed out:",
-                "-" * padding,
-                leaderboard[1],
-            ]
+            if leaderboard:
+                padding = max(
+                    len(user)
+                    for user in itertools.chain(leaderboard[0], leaderboard[1])
+                )
+                response = [
+                    "Most timed out:",
+                    "-" * padding,
+                    leaderboard[0],
+                    "-" * padding,
+                    "Longest timed out:",
+                    "-" * padding,
+                    leaderboard[1],
+                ]
+            else:
+                response = ["No timeouts yet."]
         await ctx.send("\n".join(response))
 
     @bot.event
@@ -201,16 +211,18 @@ def main() -> None:
         Update the stored dictionary of user timeouts.
         Only takes action when changing from not in timeout to in timeout.
         """
+        now = dt.datetime.now(dt.timezone.utc)
         if before.timed_out_until is not None:
             return
         if (ending := after.timed_out_until) is None:
             return
         with TIMEOUTS.open() as fp:
-            data: dict[str, tuple[int, int]] = json.load(fp)
-        existing = data.setdefault(after.name, (0, 0))
+            data: dict[str, tuple[int, int, str]] = json.load(fp)
+        existing = data.setdefault(after.name, (0, 0, after.display_name))
         data[after.name] = (
             existing[0] + 1,
-            existing[1] + ((ending - dt.datetime.utcnow()).seconds),
+            existing[1] + ((ending - now).seconds),
+            after.display_name,
         )
         with TIMEOUTS.open("w+") as fp:
             json.dump(data, fp, indent=2)
